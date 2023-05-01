c590a520211ca9480301327fb563fd8e3fca2f6d
hanxin <hanxin@yinhai.com>
2023-05-01 20:15:46 +0800

    hx五一提交

--- APP/pages/about/about.vue
+++ APP/pages/about/about.vue
@@ -1,6 +1,9 @@
 <template>
-	<view>
-		<image src="../../static/common/about.jpg" mode="widthFix"></image>
+	<view class="about-page">
+		<image src="../../static/common/about.png" mode="widthFix"></image>
+    <view class="about-content">
+      《绿色中国》自2004年创刊以来，以“弘扬生态文明，传播绿色理念，建设美丽中国”为办刊宗旨，立足林草业，面向社会，贴近生活，积极宣传党和国家的大政、方针、政策，把握正确的舆论导向，在国内外政府机构、工商企业、教育科研、新闻出版、文化艺术、医药卫生等与“绿色”相关的行业和众多领域建立了良好的信息网络，及时反映我国林草行业动态和国内外重大绿色信息，跟踪报道引领绿色潮流的时代风云人物，突出展示引领绿色时尚倡导绿色消费的时代风貌。
+    </view>
 	</view>
 </template>
 
@@ -15,7 +18,16 @@
 </script>
 
 <style lang="less">
+.about-page {
+  height: 100vh;
+  background-color: #eee;
+}
 image{
 	width:100%;
+}
+.about-content {
+  font-size: 16px;
+  text-indent: 2em;
+  text-align: justify;
 }
 </style>
